bc898670329c4bb080d107ddf8d7fe6a92145262
Paul Niemczyk <mrpndev@gmail.com>
2025-05-27 20:06:39 -0400

    useEffect Lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Auth from './components/Auth'
 import './App.css'
+import AllPokemons from './components/AllPokemons'
 
 function App() {
+
+	/* 
+		? useEffect()
+		* hook allowing us to perform side-effects in React
+		* these include: DOM manipulation; setTimeouts; and fetching data
+		* it takes a callback fx; cleanup fx; and a dependency
+		* callback fx
+			* fx that runs (by default every time a re-render occurs)
+		* cleanup fx
+			* runs after the callback fx (optional if specified)
+		* dependendencies
+			* none --> runs after every render
+			* [] (empty dependency) --> runs on initial page load ONLY
+			* [<some_dependency>] --> runs on initial page load and if the dependency changes
+
+	*/
+
+	const [ ourDependency, setOurDependency ] = useState(false)
+
+	useEffect(() => {
+		console.log("use effect triggered")
+	}, [ourDependency])
 
   // light switch state variable with initial value of false
   const [ lightSwitch, setLightSwitch ] = useState(false)
@@ -21,7 +44,9 @@ function App() {
       <button onClick={() => setLightSwitch(!lightSwitch)}>Light Switch</button>
       { lightSwitch ? <h1>The light is on</h1> : <h1>The light is off</h1>}
       {displayLight()}
+	  <button onClick={() => setOurDependency(!ourDependency)}>Dependency Switch</button>
       <Auth />
+	  <AllPokemons />
     </>
   )
 }
---
+++ src/components/AllPokemons.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react'
+
+function AllPokemons() {
+
+	const [ allPokemons, setAllPokemons ] = useState([])
+	
+	useEffect(() => {
+		if (!allPokemons.length) {
+			fetch("https://pokeapi.co/api/v2/pokemon")
+				.then(res => res.json())
+				.then(data => {
+					console.log(data)
+					setAllPokemons(data.results)
+				}, [])
+		}
+	})
+
+  return (
+	<div>{!allPokemons.length ? "Loading" : allPokemons.map((p, i) => (<h1 key={i}>{p.name}</h1>))}</div>
+  )
+}
+
+export default AllPokemons
