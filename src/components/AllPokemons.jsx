07113ed655e5c97ef9331c7b14b66c4786ab14b0
Paul Niemczyk <mrpndev@gmail.com>
2025-05-27 20:10:49 -0400

    pushed fix

--- src/components/AllPokemons.jsx
+++ src/components/AllPokemons.jsx
@@ -2,22 +2,20 @@ import React, { useState, useEffect } from 'react'
 
 function AllPokemons() {
 
-	const [ allPokemons, setAllPokemons ] = useState([])
-	
-	useEffect(() => {
-		if (!allPokemons.length) {
-			fetch("https://pokeapi.co/api/v2/pokemon")
-				.then(res => res.json())
-				.then(data => {
-					console.log(data)
-					setAllPokemons(data.results)
-				}, [])
-		}
-	})
+	const [allPokemons, setAllPokemons] = useState([])
 
-  return (
-	<div>{!allPokemons.length ? "Loading" : allPokemons.map((p, i) => (<h1 key={i}>{p.name}</h1>))}</div>
-  )
+	useEffect(() => {
+		fetch("https://pokeapi.co/api/v2/pokemon")
+			.then(res => res.json())
+			.then(data => {
+				console.log(data)
+				setAllPokemons(data.results)
+			})
+	}, [])
+
+	return (
+		<div>{!allPokemons.length ? "Loading" : allPokemons.map((p, i) => (<h1 key={i}>{p.name}</h1>))}</div>
+	)
 }
 
 export default AllPokemons
